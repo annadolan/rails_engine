ff8096d06eb5f3203684468dd1ff61a37176d74c
antciccone <aciccone@bu.edu>
2016-11-28 19:36:19 -0700

    adding show method to invoices controller with test

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :invoice do
-    customer 
+    customer
     merchant
     status "MyString"
   end
-end
+end 
--- spec/requests/api/v1/invoice_request_spec.rb
+++ spec/requests/api/v1/invoice_request_spec.rb
@@ -13,5 +13,16 @@ describe "invoices endpoints" do
       expect(invoices.count).to eq(3)
     end
   end
+  context "GET /invoices/:id" do
+    it "returns a specific invoice" do
+      invoice = create(:invoice)
 
+      get "/api/v1/invoices/#{invoice.id}"
+
+      invoices = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoices["status"]).to eq("MyString")
+    end
+  end
 end
