f73ec6fb366f3c1ac1bbfed853e38b0fd53f1439
Anna Dolan <anna.dolan@gmail.com>
2016-11-29 11:36:35 -0700

    Add routes for all relationship endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,32 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :invoices do
-        get '/', to: 'invoices#index'
-        get '/:id', to: 'invoices#show'
+        get '/:id/transactions', to: 'transactions#index'
+        get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/items', to: 'items#index'
+        get '/:id/customer', to: 'customer#show'
+        get '/:id/merchant', to: 'merchant#show'
+      end
+      namespace :items do
+        get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/merchant', to: 'merchant#show'
+      end
+      namespace :invoice_items do
+        get '/:id/invoice', to: 'invoices#show'
+        get '/:id/item', to: 'items#show'
+      end
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+        get '/:id/invoices', to: 'invoices#index'
+      end
+      namespace :transactions do
+        get '/:id/invoice', to: 'invoices#show'
+      end
+      namespace :customers do
+        get '/:id/invoices', to: 'invoices#index'
         get '/:id/transactions', to: 'transactions#index'
       end
+      resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
       resources :merchants, only: [:index, :show]
