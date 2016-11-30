05c63c16de1f38f0793c2515ad01548524109cb7
antciccone <aciccone@bu.edu>
2016-11-30 10:36:44 -0700

    adding find_all for merchants

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Items::FindController < ApplicationController
 
+  def index
+    render json: Item.where(item_params)
+  end
+
   def show
     render json: Item.find_by(item_params)
   end
--- spec/requests/api/v1/items/find_request_spec.rb
+++ spec/requests/api/v1/items/find_request_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
 
-describe "items finder" do
+describe "items find" do
   context "items/find?params" do
     it "returns item by specified param" do
       item = create(:item)
 
-      get "/api/v1/items/find?#name=#{item.name}"
+      get "/api/v1/items/find?name=#{item.name}"
 
       item_json = JSON.parse(response.body)
-      
+
       expect(response).to be_success
       expect(item_json["name"]).to eq("MyString")
+    end
+  end
+
+  context "items/find_all?params" do
+    it "returns multiple items by params" do
+      item1 = create_list(:item, 3, name: "anthony")
+      item2 = create_list(:item, 3, name: "tony")
+
+      get "/api/v1/items/find_all?name=#{item1.first.name}"
+
+      item_json = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item_json.first["name"]).to eq("anthony")
+      expect(item_json.count).to eq(3)
     end
   end
 end
