671dc5082b15e824f5e1259aecdf6c1c1acb091d
Anna Dolan <anna.dolan@gmail.com>
2016-11-30 13:48:08 -0700

    Move revenue method to model

--- app/serializers/revenue_serializer.rb
+++ app/serializers/revenue_serializer.rb
@@ -2,6 +2,6 @@ class RevenueSerializer < ActiveModel::Serializer
   attributes :revenue
 
   def revenue
-    ((object.invoices.joins(:transactions).where(transactions: {result: "success"}).joins(:invoice_items).sum("quantity * unit_price"))/100)
+    (object.find_total_revenue)/100
   end
 end
