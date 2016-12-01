73e7cb6c97c238586404ebc071d9f8e0b231f302
antciccone <aciccone@bu.edu>
2016-12-01 15:25:52 -0700

    changed string to ci_text

--- db/migrate/20161129001243_create_transactions.rb
+++ db/migrate/20161129001243_create_transactions.rb
@@ -1,10 +1,11 @@
 class CreateTransactions < ActiveRecord::Migration[5.0]
   def change
+    enable_extension 'citext'
     create_table :transactions do |t|
       t.references :invoice, foreign_key: true
       t.integer :credit_card_number
       t.integer :credit_card_expiration_date
-      t.string :result
+      t.citext :result
 
       t.timestamps
     end
