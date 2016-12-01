09350d184e891ed803c85743fdc227b32301aab1
antciccone <aciccone@bu.edu>
2016-11-30 21:34:51 -0700

    model tests for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Customer, type: :model do
+   context "relationships" do
+     it "has many invoices" do
+       customer = create(:customer)
+
+       expect(customer).to respond_to(:invoices)
+     end
+
+     it "has_many transactions" do
+       customer = create(:customer)
+
+       expect(customer).to respond_to(:transactions)
+     end
+
+     it "has_many merchants" do
+       customer = create(:customer)
+
+       expect(customer).to respond_to(:merchants)
+     end
+   end
 end
