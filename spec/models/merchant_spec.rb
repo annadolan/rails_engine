820b0af7070872c321516bafbfaa10b4abbae21a
antciccone <aciccone@bu.edu>
2016-11-30 21:49:32 -0700

    model test for merchants

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "relationships" do
+    it "has_many items" do
+      merchant = create(:merchant)
+
+      expect(merchant).to respond_to(:items)
+    end
+    it "has_many invoices" do
+      merchant = create(:merchant)
+
+      expect(merchant).to respond_to(:invoices)
+    end
+    it "has_many customers" do
+      merchant = create(:merchant)
+
+      expect(merchant).to respond_to(:customers)
+    end
+    it "has_many invoice_items" do
+      merchant = create(:merchant)
+
+      expect(merchant).to respond_to(:invoice_items)
+    end
+    it "has_many transactions" do
+      merchant = create(:merchant)
+
+      expect(merchant).to respond_to(:transactions)
+    end
+  end
 end
