6c6d20cff6838531dd5aef0a0570f8327e6f3eb6
Anna Dolan <anna.dolan@gmail.com>
2016-12-01 16:05:58 -0700

    Clean up code, a few minor fixes

--- app/controllers/api/v1/transactions/find_controller.rb
+++ app/controllers/api/v1/transactions/find_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::Transactions::FindController < ApplicationController
-
   def index
     render json: Transaction.where(merchant_params)
   end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   context "relationships" do
-    it "belongs_to" do
+    it "belongs to invoice" do
       transaction = create(:transaction)
 
-      expect(transaction).to respond_to(:transaction)
+      expect(transaction).to respond_to(:invoice)
     end
   end
 end
