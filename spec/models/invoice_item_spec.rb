bf856982bb7242fe8f4f4bf71edd1d42bd5493dc
antciccone <aciccone@bu.edu>
2016-12-01 12:02:40 -0700

    testing methods in invoiceitem model

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -6,12 +6,25 @@ describe InvoiceItem, type: :model do
        invoice_item = create(:invoice_item)
 
        expect(invoice_item).to respond_to(:item)
-     end
+      end
 
      it "belongs to invoice" do
        invoice_item = create(:invoice_item)
 
        expect(invoice_item).to respond_to(:invoice)
-     end
-   end
+      end
+    end
+
+  context "method testing" do
+    it "dollar_to_cents_one" do
+      invoice_item = create(:invoice_item, unit_price: 83333)
+
+      expect(InvoiceItem.dollar_to_cents_one("833.33").id).to eq(invoice_item.id)
+    end
+    it "dollar_to_cents_all" do
+      invoice_item = create(:invoice_item, unit_price: 83333)
+
+      expect(InvoiceItem.dollar_to_cents_all("833.33").first.id).to eq(invoice_item.id)
+    end
+  end
 end
