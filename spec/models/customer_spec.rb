e1d3632f8b92df84a342bca4a1ce104506402faf
Anna Dolan <anna.dolan@gmail.com>
2016-12-01 17:52:53 -0700

    Add model tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -20,4 +20,18 @@ describe Customer, type: :model do
        expect(customer).to respond_to(:merchants)
      end
    end
+   describe ".favorite_merchant" do
+     it "returns merchant where customer has conducted most successful transactions" do
+       customer1, customer2 = create_list(:customer, 2)
+       merchant1, merchant2 = create_list(:merchant, 2)
+       invoice1 = create(:invoice, merchant_id: merchant1.id, customer_id: customer1.id)
+       invoice2 = create(:invoice, merchant_id: merchant2.id, customer_id: customer2.id)
+       transaction = create(:transaction, invoice_id: invoice1.id, result: "success")
+       transaction = create(:transaction, invoice_id: invoice1.id, result: "success")
+       transaction = create(:transaction, invoice_id: invoice2.id, result: "success")
+
+       expect(customer1.favorite_merchant.id).to eq(merchant1.id)
+       expect(customer2.favorite_merchant.id).to eq(merchant2.id)
+     end
+   end
 end
